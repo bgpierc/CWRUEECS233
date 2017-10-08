c5a2bb5fecd6267a450161a8e567be864fb7bf68
bp0017 <bp0017.12@gmail.com>
2017-10-07 20:07:42 -0400

    trouble with Fractal3

--- HW5/Fractal3Chris.java
+++ HW5/Fractal3Chris.java
@@ -6,28 +6,24 @@ import java.awt.*;
 import java.awt.geom.*;
 import javax.swing.*;
 
-public class Fractal3 extends JPanel {
+public class Fractal3Chris extends JPanel {
     JFrame f = new JFrame();
 	int randomGeneratorSeed = 10; // Ajusts seed for randomNumberGenerator
 	Random rng = new Random(); // Random number generator
-	 int MAX_RECURSION = 1;  // maximum recursion level
+	int STOP = 100;  // When length < EPSILON, draw a line segment
 
     public static void main(String[] args){
         Fractal3 f = new Fractal3();
     }
 
-    public Fractal3(){
+    public Fractal3Chris(){
         f.add(this); // Add JPanel to JFrame
         f.setSize(1500, 1000);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Ensure that program terminates when window is closed
         f.setVisible(true); // Call paintComponent() first time
-		
-		for(int j = 0; j < 10; j++){
-			for (long i = 0; i < 100000; i++); // Delay before rerunning with new STOP value
-			f.repaint();  // Call paintComponent() again to rerun
-			MAX_RECURSION++;
-		}
-
+		for (long i = 0; i < 1000000000; i++); // Delay before rerunning with new STOP value
+		STOP = 10; // Go deeper with recursion
+		f.repaint();  // Call paintComponent() again to rerun
     }
 
     protected void paintComponent(Graphics g){
@@ -38,17 +34,15 @@ public class Fractal3 extends JPanel {
         int height = getHeight();
 		drawingArea.fill(new Ellipse2D.Double(-5, height/2-5, 10, 10));
 		drawingArea.fill(new Ellipse2D.Double(width-5, height/2-5, 10, 10));
-		randomFractal(0, height/2, width, height/2, drawingArea,1);
+		randomFractal(0, height/2, width, height/2, drawingArea);
     }
 
-	public void randomFractal(int leftX, int leftY, int rightX, int rightY, Graphics2D drawingArea, int recursionLevel)	{
+	public void randomFractal(int leftX, int leftY, int rightX, int rightY, Graphics2D drawingArea)	{
 		int midX, midY;      // Midpoints in the x and y dimensions
 		int delta;           // Amount to shift the line's midpoint up or down
 
-		if (recursionLevel >= MAX_RECURSION){
+		if ((rightX - leftX) <= STOP)
 			drawingArea.draw(new Line2D.Double(leftX, leftY, rightX, rightY));
-			System.out.println(recursionLevel);
-		}
 		else {
 			midX = (leftX + rightX) / 2;
 			midY = (leftY + rightY) / 2;
@@ -57,9 +51,8 @@ public class Fractal3 extends JPanel {
 			midY += delta;
 			drawingArea.setPaint(Color.BLACK);
 			drawingArea.fill(new Ellipse2D.Double(midX-5, midY-5, 10, 10));
-			randomFractal(leftX, leftY, midX,   midY,   drawingArea, recursionLevel++);
-			randomFractal(midX,  midY,  rightX, rightY, drawingArea, recursionLevel++);
-
+			randomFractal(leftX, leftY, midX,   midY,   drawingArea);
+			randomFractal(midX,  midY,  rightX, rightY, drawingArea);
 		}
 	}
 }
